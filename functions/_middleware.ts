e65e2e598bf40b9a19f79471c6a977e496c29151
robtec <rob.p.tec@gmail.com>
2022-07-14 20:40:39 +0100

    update

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,10 +3,19 @@ import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest = mailchannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "Customer Callback", email: "rob.p.tec@gmail.com" }],
+      to: [
+            {   name: "Customer Callback",
+                email: "rob.p.tec@gmail.com" 
+            }
+        ],
     },
   ],
-  from: { name: "Enquiry", email: "no-reply@example.com" },
+  from: { name: "Enquiry", email: "no-reply@jpservices.ie" },
+  subject: "Test Subject",
+    content: [{
+        "type": "text/plain",
+        "value": "Test message content\n\n" + content,
+    }],
   respondWith: () =>
     new Response(null, {
       status: 302,
